dd12cc7730de452b1b4f1ebcdebc0aa8426aa706
fefi95 <scct95@gmail.com>
2018-09-11 20:16:45 -0500

    Test date and time in main

--- pico_placa/test/test_main.py
+++ pico_placa/test/test_main.py
@@ -6,8 +6,14 @@ from ..main import main
 
 class TestClassMain(unittest.TestCase):
     """Checks the argv given to the main and its outputs"""
-    def test_valid_main(self):
+    def test_valid_main_can_drive(self):
         sys.argv = ["main.py", "ABC-1234", "10/09/2018", "7:30PM"]
+        with pytest.raises(SystemExit) as e:
+            main()
+        assert e.value.code == 0
+
+    def test_valid_main_can_not_drive(self):
+        sys.argv = ["main.py", "ABC-4321", "10/09/2018", "7:00PM"]
         with pytest.raises(SystemExit) as e:
             main()
         assert e.value.code == 0
@@ -30,14 +36,26 @@ class TestClassMain(unittest.TestCase):
             main()
         assert e.value.code == -2
 
-    def test_invalid_date(self):
+    def test_invalid_date1(self):
         sys.argv = ["main.py", "ABC-1234", "10/13/2018", "7:30PM"]
         with pytest.raises(SystemExit) as e:
             main()
         assert e.value.code == -3
 
-    def test_invalid_date(self):
+    def test_invalid_date2(self):
+        sys.argv = ["main.py", "ABC-1234", "10-09-2018", "7:30PM"]
+        with pytest.raises(SystemExit) as e:
+            main()
+        assert e.value.code == -3
+
+    def test_invalid_time1(self):
         sys.argv = ["main.py", "ABC-1234", "10/09/2018", "7:30P"]
+        with pytest.raises(SystemExit) as e:
+            main()
+        assert e.value.code == -3
+
+    def test_invalid_time2(self):
+        sys.argv = ["main.py", "ABC-1234", "10/09/2018", "17:30AM"]
         with pytest.raises(SystemExit) as e:
             main()
         assert e.value.code == -3
